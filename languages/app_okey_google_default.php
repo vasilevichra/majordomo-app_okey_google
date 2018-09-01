b86f318f3e8c2ceecf53e7ae59ed777813199fbc
Vasilevich Roman <roman.vasilevich@nexign-systems.com>
2018-09-01 09:59:40 +0300

    Update translation to correct variables.

--- languages/app_okey_google_default.php
+++ languages/app_okey_google_default.php
@@ -1,18 +1,8 @@
 <?php
 
 $dictionary = array(
-    'GPSTRACK_ACTION_ENTERING' => 'Entering location',
-    'GPSTRACK_ACTION_LEAVING' => 'Leaving location',
-    'GPSTRACK_ACTION_ENTERING_OR_LEAVING' => 'Arriving or Leaving location',
-    'GPSTRACK_OPTIONS' => 'Settings',
-    'GPSTRACK_APIKEY' => 'API key (optional):',
-    'GPSTRACK_MAPPROVIDER' => 'Map provider:',
-    'GPSTRACK_MAPTYPE' => 'Map type:',
-    'GPSTRACK_MAPTYPE_MAP' => 'Map',
-    'GPSTRACK_MAPTYPE_SATELLITE' => 'Satellite',
-    'GPSTRACK_MAPTYPE_HYBRID' => 'Hybrid',
-    'GPSTRACK_MAX_ACCURACY' => 'Maximum accuracy:',
-    'GPSTRACK_MAX_ACCURACY_DESC' => 'If the accuracy exceeds limit GPS value will not be saved. The default is 0 (disabled).'
+    'OKEY_GOOGLE_DIALOGFLOW_CLIENT_TOKEN' => 'Client token',
+    'OKEY_GOOGLE_DIALOGFLOW_DEVELOPER_TOKEN' => 'Developer token',
 );
 
 foreach ($dictionary as $k => $v) {
--- languages/app_okey_google_ru.php
+++ languages/app_okey_google_ru.php
@@ -1,18 +1,8 @@
 <?php
 
 $dictionary = array(
-    'GPSTRACK_ACTION_ENTERING' => 'Entering location',
-    'GPSTRACK_ACTION_LEAVING' => 'Leaving location',
-    'GPSTRACK_ACTION_ENTERING_OR_LEAVING' => 'Arriving or Leaving location',
-    'GPSTRACK_OPTIONS' => 'Settings',
-    'GPSTRACK_APIKEY' => 'API key (optional):',
-    'GPSTRACK_MAPPROVIDER' => 'Map provider:',
-    'GPSTRACK_MAPTYPE' => 'Map type:',
-    'GPSTRACK_MAPTYPE_MAP' => 'Map',
-    'GPSTRACK_MAPTYPE_SATELLITE' => 'Satellite',
-    'GPSTRACK_MAPTYPE_HYBRID' => 'Hybrid',
-    'GPSTRACK_MAX_ACCURACY' => 'Maximum accuracy:',
-    'GPSTRACK_MAX_ACCURACY_DESC' => 'If the accuracy exceeds limit GPS value will not be saved. The default is 0 (disabled).'
+    'OKEY_GOOGLE_DIALOGFLOW_CLIENT_TOKEN' => 'Токен клиета',
+    'OKEY_GOOGLE_DIALOGFLOW_DEVELOPER_TOKEN' => 'Токен разработчика',
 );
 
 foreach ($dictionary as $k => $v) {
