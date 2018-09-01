0506112c6f9476b17699bcd887f153336c70616f
Vasilevich Roman <roman.vasilevich@nexign-systems.com>
2018-09-01 09:46:16 +0300

    Update header of main receiving php script.

--- okey_google.php
+++ okey_google.php
@@ -3,8 +3,8 @@
  * Main project script
  *
  * @package MajorDoMo
- * @author Serge Dzheigalo <jey@tut.by> http://smartliving.ru/
- * @version 1.1
+ * @author Vasilevich Roman <vasilevich@gmail.com> https://t.me/vasilevichra
+ * @version 0.1
  */
 
 echo "ok google";
